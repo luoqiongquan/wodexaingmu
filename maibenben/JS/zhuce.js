ae5a2398c0fdb4f1ff6566d00927a99e3228b660
Your 11 <you@example.com>
2019-08-08 20:48:30 +0800

    xxx

---
+++ maibenben/JS/zhuce.js
@@ -0,0 +1,13 @@
+$(function(){
+    $("btn").click(function(){
+
+    
+ var txtName=$("input:text").val();
+   if(txtName==""){
+       alert("手机号码不能为空！");
+       return;
+       
+   }
+})
+  
+});
