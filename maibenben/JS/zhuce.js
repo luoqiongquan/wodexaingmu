eb5f1b088ace2db78b20a0575942568ca3c3f19b
Your 11 <you@example.com>
2019-08-09 09:11:15 +0800

    11

--- maibenben/JS/zhuce.js
+++ maibenben/JS/zhuce.js
@@ -1,12 +1,19 @@
 $(function(){
-    $("btn").click(function(){
-        alert(111);
-        
+    $("#btn").click(function(){
+        var TEL_REGEXP=/^1[3456789]\d{9}$/;
         if($("#inputMsg").val()==""){
-            alert(222);
             $("#phone").html("*您输入的电话号码为空！请重新输入");
+        }else{
+            if(!TEL_REGEXP.test($("#inputMsg").val())){
+                $("#phone").html('*请输入正确的手机号码');
+            }else{
+                $("#phone").html("");
+            }
+
         }
-    
+            
+
+       
  
 })
   
